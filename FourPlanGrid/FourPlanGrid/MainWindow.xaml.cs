d2f97a5a618bde86efb30cb473722dd838b122b1
Nelson Townsend <nelson.townsend@gmail.com>
2015-12-09 01:51:05 -0600

    Initial commit. Took some notes and thought about problem. Played around with XAML a bit and worked out the beginnings of a simple UI.

---
+++ FourPlanGrid/FourPlanGrid/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FourPlanGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Player1NameGotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Resources["rowHeight"] = new GridLength(50);
+        }
+    }
+}
